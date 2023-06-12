b9d56c2c90997ca9445e27cd76fb9840ccce9bca
Brayan-Alexis <brayanzxd@gmail.com>
2023-06-11 19:10:02 -0700

    Add mobile menu in navbar

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-// const navbarEmail = document.querySelector('.navbar-email');
+// const menuEmail = document.querySelector('.navbar-email');
 // const desktopMenu = document.querySelector('.desktop-menu');
 const $ = (selector) => document.querySelector(selector); // Shortcut for querySelector
 
-const navbarEmail = $('.navbar-email'); 
+const menuEmail = $('.navbar-email'); 
 const desktopMenu = $('.desktop-menu');
+const menuIcon = $('.menu');
+const mobileMenu = $('.mobile-menu');
 
 // // Toggle menu on click
 // function toggleDesktopMenu() {
@@ -12,6 +14,9 @@ const desktopMenu = $('.desktop-menu');
 
 // Toggle menu on click
 const toggleDesktopMenu = () => desktopMenu.classList.toggle('inactive');
+const toggleMobileMenu = () => mobileMenu.classList.toggle('inactive');
 
-// Add event listener to navbarEmail
-navbarEmail.addEventListener('click', toggleDesktopMenu);
+// Add event listener to menuEmail to toggle desktop menu
+menuEmail.addEventListener('click', toggleDesktopMenu);
+// Add event listener to menuIcon to toggle mobile menu
+menuIcon.addEventListener('click', toggleMobileMenu);
