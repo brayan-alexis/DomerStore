7a8082c4bda2c24e38cd85e340d16d7dc8d54343
Brayan-Alexis <brayanzxd@gmail.com>
2023-06-11 23:11:59 -0700

    Add cart in navbar

--- main.js
+++ main.js
@@ -2,21 +2,33 @@
 // const desktopMenu = document.querySelector('.desktop-menu');
 const $ = (selector) => document.querySelector(selector); // Shortcut for querySelector
 
-const menuEmail = $('.navbar-email'); 
+// Menu icons
+const menuIcon = $('.menu'); 
+const menuEmail = $('.navbar-email');
+const cartIcon = $('.navbar-shopping-cart');
+// Menus
+const mobileMenu = $('.mobile-menu'); 
 const desktopMenu = $('.desktop-menu');
-const menuIcon = $('.menu');
-const mobileMenu = $('.mobile-menu');
-
-// // Toggle menu on click
-// function toggleDesktopMenu() {
-//     desktopMenu.classList.toggle('inactive');
-// }
+const productDetail = $('.product-detail'); // Aside product detail
 
 // Toggle menu on click
-const toggleDesktopMenu = () => desktopMenu.classList.toggle('inactive');
-const toggleMobileMenu = () => mobileMenu.classList.toggle('inactive');
+const toggleMobileMenu = () => { 
+    productDetail.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive'); 
+} 
+const toggleDesktopMenu = () => {
+    productDetail.classList.add('inactive');
+    desktopMenu.classList.toggle('inactive');
+}
+const toggleProductDetail = () => {
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    productDetail.classList.toggle('inactive');
+}
 
-// Add event listener to menuEmail to toggle desktop menu
-menuEmail.addEventListener('click', toggleDesktopMenu);
 // Add event listener to menuIcon to toggle mobile menu
 menuIcon.addEventListener('click', toggleMobileMenu);
+// Add event listener to menuEmail to toggle desktop menu
+menuEmail.addEventListener('click', toggleDesktopMenu);
+// Add event listener to cartIcon to toggle product detail
+cartIcon.addEventListener('click', toggleProductDetail);
