f6b57307d61076bf31ee465df673dcc09d4b44fc
Brayan-Alexis <brayanzxd@gmail.com>
2023-06-12 19:58:29 -0700

    Add product list and render function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // const desktopMenu = document.querySelector('.desktop-menu');
 const $ = (selector) => document.querySelector(selector); // Shortcut for querySelector
 
-// Menu icons
+// Nanbar menu icons
 const menuIcon = $('.menu'); 
 const menuEmail = $('.navbar-email');
 const cartIcon = $('.navbar-shopping-cart');
@@ -10,6 +10,8 @@ const cartIcon = $('.navbar-shopping-cart');
 const mobileMenu = $('.mobile-menu'); 
 const desktopMenu = $('.desktop-menu');
 const productDetail = $('.product-detail'); // Aside product detail
+// Containers
+const cardsContainer = $('.cards-container'); // Container for product cards
 
 // Toggle menu on click
 const toggleMobileMenu = () => { 
@@ -32,3 +34,60 @@ menuIcon.addEventListener('click', toggleMobileMenu);
 menuEmail.addEventListener('click', toggleDesktopMenu);
 // Add event listener to cartIcon to toggle product detail
 cartIcon.addEventListener('click', toggleProductDetail);
+
+const productList = []; // List of products
+
+// Add products to productList
+productList.push({
+    id: '1',
+    name: 'Basic White T-Shirt', 
+    price: 25,
+    image: 'https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    id: '2',
+    name: 'Basic Black T-Shirt',
+    price: 25,
+    image: 'https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+productList.push({
+    id: '3',
+    name: 'Basic Gray T-Shirt',
+    price: 25,
+    image: 'https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+});
+
+// Function to render products
+function renderProducts(productList){
+    // Create product cards
+    for (product of productList) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImage = document.createElement('img');
+        productImage.src = product.image;
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.textContent = `$${product.price}.00`;
+        const productName = document.createElement('p');
+        productName.textContent = product.name;
+        productInfoDiv.append(productPrice, productName); // Add productPrice and productName to productInfoDiv
+
+        const productFigure = document.createElement('figure');
+        const productFigureImg = document.createElement('img');
+        productFigureImg.src = 'assets/icons/bt_add_to_cart.svg';
+        productFigure.appendChild(productFigureImg); // Add productFigureImg to productFigure
+
+        productInfo.append(productInfoDiv, productFigure); // Add productInfoDiv and productFigure to productInfo
+        productCard.append(productImage, productInfo); // Add productImage and productInfo to productCard
+        cardsContainer.appendChild(productCard); // Add productCard to cardsContainer
+    }
+}
+
+// Render products
+renderProducts(productList);
