894534009e4b59b960258b31ab6fb635797c6303
Brayan-Alexis <brayanzxd@gmail.com>
2023-08-10 21:30:56 -0700

    Implement product detail rendering

--- main.js
+++ main.js
@@ -62,37 +62,43 @@ productList.push({
     id: '1',
     name: 'Basic White T-Shirt', 
     price: 10,
-    image: 'https://images.pexels.com/photos/4101142/pexels-photo-4101142.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    image: 'https://images.pexels.com/photos/4101142/pexels-photo-4101142.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'This is a high-quality white t-shirt made from soft cotton fabric. It\'s a versatile and essential piece for your wardrobe.'
 });
 productList.push({
     id: '2',
     name: 'White T-Shirt Minimalist Design',
     price: 20,
-    image: 'https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    image: 'https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'This is a high-quality white t-shirt with minimalist design made from soft cotton fabric. It\'s a versatile and essential piece for your wardrobe.'
 });
 productList.push({
     id: '3',
     name: 'White T-Shirt with Basic logo',
     price: 25,
-    image: 'https://images.pexels.com/photos/14769479/pexels-photo-14769479.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    image: 'https://images.pexels.com/photos/14769479/pexels-photo-14769479.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'This is a high-quality white t-shirt with Basic logo made from soft cotton fabric. It\'s a versatile and essential piece for your wardrobe.'
 });
 productList.push({
     id: '4',
     name: 'Blue T-Shirt with Superman logo',
     price: 30,
-    image: 'https://images.pexels.com/photos/8249696/pexels-photo-8249696.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    image: 'https://images.pexels.com/photos/8249696/pexels-photo-8249696.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'This is a high-quality blue t-shirt made from soft cotton fabric. It\'s a versatile and essential piece for your wardrobe.'
 });
 productList.push({
     id: '5',
     name: 'Color blouse',
     price: 35,
-    image: 'https://images.pexels.com/photos/11843404/pexels-photo-11843404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    image: 'https://images.pexels.com/photos/11843404/pexels-photo-11843404.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'This is a high-quality color blouse made from soft cotton fabric. It\'s a versatile and essential piece for your wardrobe.'
 });
 productList.push({
     id: '6',
     name: 'White T-Shirt with Superman logo',
     price: 30,
-    image: 'https://images.pexels.com/photos/8249697/pexels-photo-8249697.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+    image: 'https://images.pexels.com/photos/8249697/pexels-photo-8249697.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'This is a high-quality white t-shirt with Superman logo made from soft cotton fabric. It\'s a versatile and essential piece for your wardrobe.'
 });
 
 // Function to render products
@@ -101,6 +107,7 @@ function renderProducts(productList){
     for (product of productList) {
         const productCard = document.createElement('div');
         productCard.classList.add('product-card');
+        productCard.dataset.productId = product.id;
         productCard.addEventListener('click', showProductDetail);
 
         const productImage = document.createElement('img');
@@ -129,3 +136,70 @@ function renderProducts(productList){
 }
 // Render products
 renderProducts(productList);
+
+// Function to show product detail container
+function showProductDetail (){
+    desktopMenu.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+    const productId = this.dataset.productId; // Get the productId from the clicked product card
+    const product = productList.find(item => item.id === productId); // Find the product by its id
+    if (product) {
+        renderProductDetail(product); // Call a function to render the product details in the container
+    }
+}
+
+// Function to render product detail in the productDetailContainer
+function renderProductDetail(product) {
+    // const productDetail = document.createElement('div');
+    // productDetail.classList.add('product-detail');
+
+    // Close button for product detail container
+    const productDetailClose = document.createElement('div');
+    productDetailClose.classList.add('product-detail-close');
+    productDetailClose.addEventListener('click', hideProductDetailContainer);
+    const productDetailCloseImg = document.createElement('img');
+    productDetailCloseImg.src = './assets/icons/icon_close.png';
+    productDetailCloseImg.alt = 'close';
+    productDetailClose.appendChild(productDetailCloseImg);
+
+    const productImage = document.createElement('img');
+    productImage.src = product.image;
+    productImage.alt = product.name;
+
+    const divProductDetail = document.createElement('div');
+    divProductDetail.classList.add('product-info');
+
+    const productName = document.createElement('h2');
+    productName.textContent = product.name;
+
+    const productPrice = document.createElement('p');
+    productPrice.textContent = `$${product.price}.00`;
+
+    const productDescription = document.createElement('p');
+    productDescription.textContent = product.description;
+
+    // Button to add product to cart
+    const productFigure = document.createElement('figure');
+    const productFigureImg = document.createElement('img');
+    productFigureImg.src = 'assets/icons/bt_add_to_cart.svg';
+    const buttonAddToCart = document.createElement('button');
+    buttonAddToCart.classList.add('primary-button', 'add-to-cart-button');
+    buttonAddToCart.textContent = 'Add to cart';
+    buttonAddToCart.appendChild(productFigureImg); // Add productFigureImg to buttonAddToCart
+    productFigure.appendChild(buttonAddToCart); // Add productFigure to buttonAddToCart
+
+    divProductDetail.append(productName, productPrice, productDescription, productFigure); // Add productName, productPrice, productDescription, and productFigure to divProductDetail
+
+    // Add productImage, productName, and productPrice to the productDetail
+    //productDetail.append(productImage, divProductDetail);
+
+    // Clear the previous content of productDetailContainer
+    //productDetailContainer.innerHTML = '';
+
+    // Add productDetail to the productDetailContainer
+    //productDetailContainer.appendChild(productDetail);
+    productDetailContainer.appendChild(productDetailClose);
+    productDetailContainer.appendChild(productImage);
+    productDetailContainer.appendChild(divProductDetail);
+}
